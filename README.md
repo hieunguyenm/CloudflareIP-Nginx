a0680a46f89977d097bc7bc9dd4f4b15eb10daa8
Hieu Nguyen <hieunguyenm@users.noreply.github.com>
2017-06-02 12:44:41 +0100

    Added script and updated README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# cloudflareip-nginx
+# CloudflareIP-Nginx 
+
 Restore original visitor IP instead of Cloudflare IP in Nginx
+
+The script requires wget, sed and printf (Tested on Ubuntu 16.04, these packages are pre-installed).
+
+## Running the script
+
+1. Set the script to executable
+
+```sh
+sudo chmod +x cloudflare.sh
+```
+
+2. Run script as sudo
+
+```sh
+sudo ./cloudflare.sh
+```
+
+3. (Optional) Add script to cron to run weekly
+
+```sh
+sudo crontab -e
+```
+
+* Add the following line
+
+	```
+	0 0 * * 0 root <path to script>
+	```
---
+++ cloudflare.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Download Cloudflare IPs and save to /etc/nginx/cloudflare
+wget -q https://www.cloudflare.com/ips-v4 -O - > /etc/nginx/cloudflare.conf
+wget -q https://www.cloudflare.com/ips-v6 -O - >> /etc/nginx/cloudflare.conf
+
+# Use sed to write nginx syntax lines to file
+sed -i 's/^/set_real_ip_from /' /etc/nginx/cloudflare.conf
+sed -i 's/$/;/' /etc/nginx/cloudflare.conf
+
+# Append real_ip_header to file
+printf "\nreal_ip_header CF-Connecting-IP;\n" >> /etc/nginx/cloudflare.conf
+
+# Reload nginx
+/usr/sbin/nginx -s reload
